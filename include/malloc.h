36a51d22f64ee3361941773d7002c034a36c2ec1
Your Name <you@example.com>
2014-04-16 04:14:00 +0200

    patch

--- include/malloc.h
+++ include/malloc.h
@@ -3,6 +3,15 @@
 
 # include <unistd.h>
 
+typedef	struct s_mal t_mal;
+struct		s_mal
+{
+	t_mal		*prev;
+	t_mal		*next;
+	size_t		total;
+	size_t		dipo;
+};
+
 void	*malloc(size_t size);
 
 #endif
--- src/malloc.c
+++ src/malloc.c
@@ -1,8 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "malloc.h"
+#include <sys/mman.h>
+
 
 void	*malloc(size_t size)
 {
-	(void)size;
+	size_t		n;
+	size_t		m;
+
+	n = 4096;
+	m = 40000;
+	if (size < n)
+		;
+	else if (size < m)
+		;
+	else
+		;
 	return (NULL);
 }
